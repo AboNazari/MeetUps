d30b564b8c747c8eb0f24dd889f5b5460f31af11
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-01-22 08:16:05 +0600

    global navbar added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,6 @@
-import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
+import Link from "next/router";
 
 function Layout(props) {
   return (
