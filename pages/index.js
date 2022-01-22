12e79a1ccbae148f1b03a677b49dba4a11f32dd4
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-01-22 07:54:36 +0600

    HomePage added

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const Dummy_Meetups = [
+  {
+    id: "1",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.lrEfWvPVD4P8zrVwIr6xSQHaEo%26pid%3DApi&f=1",
+    title: "First Meetup",
+    address: "somewhere 110 st city",
+    description: "This is first meetup",
+  },
+  {
+    id: "2",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.uSaEGYBQHosLJYLK1CPWrQHaE8%26pid%3DApi&f=1",
+    title: "Second Meetup",
+    address: "somewhere 310 st city",
+    description: "This is Second meetup",
+  },
+  {
+    id: "3",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.BvFhZTRcaAyMIIZKiMqZwgHaEo%26pid%3DApi&f=1",
+    title: "Third Meetup",
+    address: "somewhere 210 st city",
+    description: "This is Third meetup",
+  },
+];
+
+const HomePage = () => {
+  return <MeetupList meetups={Dummy_Meetups} />;
+};
+
+export default HomePage;
