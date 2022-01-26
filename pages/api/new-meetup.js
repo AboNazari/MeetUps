6629df5e7e2c57ca5c73bb9dd320353a306cb1f8
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-01-26 07:58:48 +0600

    mongodb connection built

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,19 @@
 //POST /api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
-  } 
+    const client = await MongoClient.connect(
+      "mongodb+srv://Abdullah:h234asdf@asdf%#asdf@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupCollection = db.collection("meetups");
+    const result = await meetupCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup Inserted!" });
+  }
 }
 
 export default handler;
