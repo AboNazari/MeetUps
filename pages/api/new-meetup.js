b1a3ca7090186dbf6f2c83de6c5a05fe368ddfa1
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-01-26 08:14:19 +0600

    try catch block added

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,18 +1,23 @@
 //POST /api/new-meetup
+import { error } from "console";
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const client = await MongoClient.connect(
-      "mongodb+srv://Abdullah:h234asdf@asdf%#asdf@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority"
-    );
-    const db = client.db();
-    const meetupCollection = db.collection("meetups");
-    const result = await meetupCollection.insertOne(data);
-    console.log(result);
-    client.close();
-    res.status(201).json({ message: "Meetup Inserted!" });
+    try {
+      const client = await MongoClient.connect(
+        "mongodb+srv://Abdullah:h234asdf@asdf%#asdf@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority"
+      );
+      const db = client.db();
+      const meetupCollection = db.collection("meetups");
+      const result = await meetupCollection.insertOne(data);
+      console.log(result);
+      client.close();
+      res.status(201).json({ message: "Meetup Inserted!" });
+    } catch (err) {
+      res.status(400).json({ message: "something went wronge!" });
+    }
   }
 }
 
