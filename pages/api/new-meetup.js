9b935866ad6cb1538a8c1fecb4e6421bb831196b
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-01-27 18:38:32 +0600

    edited api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,23 +1,17 @@
 //POST /api/new-meetup
-import { error } from "console";
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    try {
-      const client = await MongoClient.connect(
-        "mongodb+srv://Abdullah:h234asdf@asdf%#asdf@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority"
-      );
-      const db = client.db();
-      const meetupCollection = db.collection("meetups");
-      const result = await meetupCollection.insertOne(data);
-      console.log(result);
-      client.close();
-      res.status(201).json({ message: "Meetup Inserted!" });
-    } catch (err) {
-      res.status(400).json({ message: "something went wronge!" });
-    }
+    const client = await MongoClient.connect(
+      "mongodb+srv://Abdullah:h234asdf@asdf%#asdf@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupCollection = db.collection("meetups");
+    const result = await meetupCollection.insertOne(data);
+    client.close();
+    res.status(201).json({ message: "Meetup Inserted!" });
   }
 }
 
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,20 @@
 import MeetupForm from "../../components/meetups/NewMeetupForm";
+import { useRouter } from "next/router";
 
 const NewMeetups = () => {
-  const meetupsFormHandler = (meetupsEntry) => {
-    console.log(meetupsEntry);
+  const router = useRouter();
+  const meetupsFormHandler = async (meetupsEntry) => {
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(meetupsEntry),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data.message);
+
+    router.push("/");
   };
 
   return <MeetupForm onAddMeetup={meetupsFormHandler} />;
