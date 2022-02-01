1c9533c1b661dc97ed5c995b5063f7dad235fd9a
Abdullah Nazari <abdullahnazari2001@gmail.com>
2022-02-01 09:33:25 +0600

    mongopass managed

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,12 +1,13 @@
 //POST /api/new-meetup
 import { MongoClient } from "mongodb";
+import { secret } from "../../secret";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
     try {
       const client = await MongoClient.connect(
-        "mongodb+srv://Abdullah:h234asdf@asdf%#asdf@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority"
+        `mongodb+srv://Abdullah:${secret.mongoPass}@clusterabo.c9i6p.mongodb.net/meetups?retryWrites=true&w=majority`
       );
       const db = client.db();
 
@@ -19,7 +20,7 @@ export default async function handler(req, res) {
       client.close();
       res.status(200).json({ message: "Meetup Inserted!" });
     } catch (err) {
-      res.status(500).json({ message: "failed to load data" });
+      res.status(500).json({ message: "failed to load data!" });
     }
   }
 }
